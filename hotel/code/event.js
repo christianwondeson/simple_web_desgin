1a6d745502cf54b9a3071c048185faff96defc45
chris_wendi <72356458+christianwondeson@users.noreply.github.com>
2021-11-30 21:33:40 +0300

    Add files via upload

---
+++ hotel/code/event.js
@@ -0,0 +1,18 @@
+
+      var x = document.getElementById("login");
+      //finding element by their id then retuning it to there respective variable
+      var y = document.getElementById("register");
+      var z = document.getElementById("Button");
+
+
+      function register() {
+        x.style.left = "-400px";
+        y.style.left = "50px";
+        z.style.left = "110px";
+      }
+      function login()
+      {
+        x.style.left = "50px";
+        y.style.left = "450px";
+        z.style.left="0px";
+      }
